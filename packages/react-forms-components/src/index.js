799312fd760d63c54d09bd90a65f0c4823567cbd
DESKTOP-BETK9LD\Ale <aguemat@gmail.com>
2020-11-30 14:48:13 +0100

    Change mode export

--- packages/react-forms-components/src/index.js
+++ packages/react-forms-components/src/index.js
@@ -1,11 +1,11 @@
 export { default as InputText } from "./InputText";
 export { default as InputTextFormik } from "./InputText/formik";
 
-export { default as InputCheck } from "./InputCheck";
-export { default as InputCheckFormik } from "./InputCheck/formik";
+export { InputCheck } from "./InputCheck";
+export { InputCheckFormik } from "./InputCheck/formik";
 
 export { default as InputImage } from "./InputImage";
 export { default as InputImageFormik } from "./InputImage/formik";
 
-export { default as InputSelect } from "./InputSelect";
-export { default as InputSelectFormik } from "./InputSelect/formik";
+export { InputSelect } from "./InputSelect";
+export { InputSelectFormik } from "./InputSelect/formik";
